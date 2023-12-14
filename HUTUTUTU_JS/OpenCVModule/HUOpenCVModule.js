fcaedbc02b5491931787b912e9c45a273c958e73
hu jiawen <644727408@qq.com>
2023-12-14 14:26:49 +0800

    计算图片直方图

--- HUTUTUTU_JS/OpenCVModule/HUOpenCVModule.js
+++ HUTUTUTU_JS/OpenCVModule/HUOpenCVModule.js
@@ -119,39 +119,36 @@ const HUOpenCVModule = {
 
   },
 
-  // 计算直方图
-  drawHistogram(mat) {
+  calcHist(mat) {
     const src = new cv.Mat();
-    cv.cvtColor(mat, src, cv.COLOR_RGBA2HSV, 0);
+    cv.cvtColor(mat, src, cv.COLOR_RGBA2GRAY, 0);
     let srcVec = new cv.MatVector();
     srcVec.push_back(src);
-    let accumulate = false;
-    let channels = [0];
-    let histSize = [256];
-    let ranges = [0, 255];
     let hist = new cv.Mat();
-    let mask = new cv.Mat();
-    let color = new cv.Scalar(255, 255, 255);
-    let scale = 2;
+    let histSize = [256];
+    let ranges = [0, 256];
+    let channels = [0];
     // You can try more different parameters
-    cv.calcHist(srcVec, channels, mask, hist, histSize, ranges, accumulate);
-    let result = cv.minMaxLoc(hist, mask);
-    let max = result.maxVal;
-    let dst = new cv.Mat.zeros(src.rows, histSize[0] * scale,
-      cv.CV_8UC3);
-    // draw histogram
-    for (let i = 0; i < histSize[0]; i++) {
-      let binVal = hist.data32F[i] * src.rows / max;
-      let point1 = new cv.Point(i * scale, src.rows - 1);
-      let point2 = new cv.Point((i + 1) * scale - 1, src.rows - binVal);
-      cv.rectangle(dst, point1, point2, color, cv.FILLED);
+    cv.calcHist(srcVec, channels, new cv.Mat(), hist, histSize, ranges);
+    return hist;
+  },
+  // 绘制直方图
+  drawHistogram(mat) {
+    const hist = this.calcHist(mat);
+    let minMax = cv.minMaxLoc(hist);
+    // 高度固定为 144 + 20，144 是宽高比为 16：9 计算得来。宽度固定为 256
+    const height = 144 + 20;
+    const scale = minMax.maxVal / 144;
+
+    let histogram = new cv.Mat.zeros(height, 256, cv.CV_8U);
+    for (let i = 0; i < 256; i++) {
+      let binVal = parseInt(hist.data32F[i] / scale) + 10;
+      let point1 = new cv.Point(i, height - binVal);
+      let point2 = new cv.Point(i + 1, height);
+      cv.rectangle(histogram, point1, point2, new cv.Scalar(255), cv.FILLED);
     }
-    src.delete();
-    // dst.delete();
-    srcVec.delete();
-    mask.delete();
-    hist.delete();
-    return dst;
+    cv.cvtColor(histogram, histogram, cv.COLOR_GRAY2RGBA, 0);
+    return histogram;
   }
 }
 
